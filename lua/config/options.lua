51aa8e5096a768224c23a69313bffd421562633c
Aryah Kannan <aryah.kannan@cartenav.com>
2025-09-04 17:46:24 -0300

    powershell terminal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,29 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.fixendofline = false
 vim.cmd("set guicursor=")
 vim.cmd("set number")
+
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
+end
+
+-- Setting shell command flags
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
